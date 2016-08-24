43e0cf3d39331c4efa1fde6514f94bdbb93f1974
Jorge Delgado <ajffdelgado@hotmail.com>
2016-08-24 09:54:54 -0300

    Service

---
+++ controllers/service.js
@@ -0,0 +1,48 @@
+angular.module('aula3')
+.factory('Requisicao', ['$http','$rootScope', function($http,$rootScope) {
+   var ip = ""
+   var config = {headers: {
+	          'Content-Type': 'application/json',
+	          'Accept': 'application/json'
+	        }
+
+   }
+   return{
+   		cadastrar:function(data){
+   			$http({
+	        method: 'POST',
+	        data: data,
+	        url:ip+"/usuarios",
+	        headers: {
+	          'Content-Type': 'application/json',
+	          'Accept': 'application/json'
+	        }
+	      }).then(function successCallback(response) {
+	        console.log(response)
+
+	      },
+	      function errorCallback(response) {
+	        //$.toast('Erro ao identificar localização atual.', {sticky: true, type: 'danger'});                
+	      });
+   		},
+   		listar:function(){
+   			$http({
+	        method: 'GET',
+	        url:ip+"/usuarios",
+	        headers: {
+	          'Content-Type': 'application/json',
+	          'Accept': 'application/json'
+	        }
+	      }).then(function successCallback(response) {
+	        console.log(response)
+	        $rootScope.cadastrados= response.data;
+
+	      },
+	      function errorCallback(response) {
+	        //$.toast('Erro ao identificar localização atual.', {sticky: true, type: 'danger'});                
+	      });
+	      //$http.get(ip+'/usuarios', config).success(success).error(error)
+
+   		}
+   }
+ }]);
